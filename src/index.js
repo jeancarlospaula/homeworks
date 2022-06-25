b3df2e2842f1a0b2c58e1485e5a31dec3ec82a9c
jean.depaula <jean.depaula@obraprimaweb.com.br>
2022-06-25 11:05:37 -0300

    chore: HMW-1 - Added routes in index and changed some project settings

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
-const connectDB = require('./database/connection/connect')
+const connectDB = require('./database/connection/connectDB.js')
 const express = require('express')
+const routes = require('./routes/routes')
 const app = express()
 const port = process.env.PORT || process.env.PORT_LOCALHOST
 
 const start = async () => {
   await connectDB()
+  app.use('/', routes)
   app.listen(port, () => console.log(`Server active on port ${port}`))
 }
 
