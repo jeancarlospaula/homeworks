64952a7c936d7b63e48e23bb936f231210ccb0ed
jean.depaula <jean.depaula@obraprimaweb.com.br>
2022-06-25 11:07:00 -0300

    chore HMW-1 - Added functions to handle errors and validate body schema

---
+++ src/middlewares/handlerRequestError.js
@@ -0,0 +1,8 @@
+const handlerRequestError = (error, req, res, next) => {
+  if (error instanceof Error) {
+    console.log(error)
+    return res.status(error.status).json({ error: { message: error.message } })
+  }
+}
+
+module.exports = handlerRequestError
---
+++ src/utils/bodySchema/bodySchemas.js
@@ -0,0 +1,5 @@
+const registerSchema = ['firstName', 'lastName', 'email', 'password']
+
+module.exports = {
+  registerSchema
+}
