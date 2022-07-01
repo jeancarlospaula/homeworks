91ee61afad2f73a579bb150c1c6129eac077413a
jean.depaula <jean.depaula@obraprimaweb.com.br>
2022-06-30 21:24:59 -0300

    chore: HMW-5 - Added request schema for password reset and update by id function in user repository

--- src/utils/bodySchema/bodySchemas.js
+++ src/utils/bodySchema/bodySchemas.js
@@ -1,7 +1,11 @@
 const registerSchema = ['firstName', 'lastName', 'email', 'password']
 const confirmEmailSchema = ['email']
+const resetPasswordEmailSchema = ['email']
+const resetPasswordSchema = ['password']
 
 module.exports = {
   registerSchema,
-  confirmEmailSchema
+  confirmEmailSchema,
+  resetPasswordEmailSchema,
+  resetPasswordSchema
 }
