419cd4917a8ffdec5e9755056dc6b1fb106d5024
jean.depaula <jean.depaula@obraprimaweb.com.br>
2022-08-02 19:03:06 -0300

    chore: HMW-7 - added middleware to validate user header token

---
+++ src/middlewares/validateToken.js
@@ -0,0 +1,61 @@
+const jwt = require('jsonwebtoken')
+const Blacklist = require('../repositories/blacklist.repository')
+const errorManager = require('../utils/errors/errorManager')
+const errorThrower = require('../utils/errors/errorThrower')
+
+const validateToken = async (req, res, next) => {
+  try {
+    const token = req.headers['x-access-token']
+
+    if (!token) {
+      errorThrower({
+        message: 'Valid x-access-token header is required',
+        statusCode: 401
+      })
+    }
+
+    const isTokenBlacklisted = await Blacklist.findOne({ token })
+
+    if (isTokenBlacklisted) {
+      errorThrower({
+        message: 'Expired token',
+        statusCode: 401
+      })
+    }
+
+    const { user } = jwt.verify(token, process.env.SECRET_JWT)
+
+    req.user = user
+
+    next()
+  } catch (error) {
+    if (error.message === 'invalid token') {
+      return res.status(401).json({
+        error: {
+          message: 'Valid x-access-token header is required'
+        }
+      })
+    }
+
+    if (error.name === 'jwt expired') {
+      return res.status(401).json({
+        error: {
+          message: 'Expired token'
+        }
+      })
+    }
+
+    const response = errorManager({
+      error,
+      genericMessage: 'Error validating token. Try again later.'
+    })
+
+    if (error.statusCode) {
+      return res.status(error.statusCode).json(response)
+    }
+
+    return res.status(400).json(response)
+  }
+}
+
+module.exports = { validateToken }
