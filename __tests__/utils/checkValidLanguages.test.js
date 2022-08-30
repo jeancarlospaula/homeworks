db848e8ce7733d49065f2cdb2125a53fec657fec
jean.depaula <jean.depaula@obraprimaweb.com.br>
2022-08-30 13:10:50 -0300

    test: HMW-18 - Added funtion to validate languages

---
+++ __tests__/utils/checkValidLanguages.test.js
@@ -0,0 +1,19 @@
+const { checkValidLanguages } = require('../../src/utils/checkValidLanguages')
+
+describe('checkValidLanguages', () => {
+  it('should throw an error when the language is not valid', () => {
+    const language = 'invalid'
+
+    expect(() => checkValidLanguages({ language })).toThrow()
+  })
+
+  it('should not throw an error when the language is valid', () => {
+    const portuguese = 'pt'
+    const english = 'en'
+    const spanish = 'es'
+
+    expect(() => checkValidLanguages({ language: portuguese })).not.toThrow()
+    expect(() => checkValidLanguages({ language: english })).not.toThrow()
+    expect(() => checkValidLanguages({ language: spanish })).not.toThrow()
+  })
+})
