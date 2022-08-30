826d5c07875006dfa3e9c95519090d6f9f92e429
jean.depaula <jean.depaula@obraprimaweb.com.br>
2022-08-30 13:13:00 -0300

    chore: HMW-18 - Added funtion to create a template of email of report tasks

---
+++ src/templates/tasksReportEmail.js
@@ -0,0 +1,21 @@
+const { images } = require('./images/images.js')
+
+const htmlTasksReportEmail = ({ name }) => {
+  return `<div>
+      <p>Hi ${name}, here is your <b>PDF task report</b> attached.</p>
+      <br/>
+      <img src='${images.emailBanner}' alt='HomeWorks Logo' height='50'/>
+      <br/>
+      <br/>
+      <p>This is an automated email, please do not reply.</p>
+    </div>`
+}
+
+const textTasksReportEmail = ({ name }) => {
+  return `Hi ${name} here is your <b>PDF task report</b> attached.\nThis is an automated email, please do not reply.`
+}
+
+module.exports = {
+  htmlTasksReportEmail,
+  textTasksReportEmail
+}
