bee300e6f4a595b6e60971cae523f609a24afd6d
jean.depaula <jean.depaula@obraprimaweb.com.br>
2022-08-30 13:12:18 -0300

    test: HMW-18 - Added funtion to sort an array of dates

---
+++ __tests__/utils/sortDatesArray.test.js
@@ -0,0 +1,14 @@
+const { sortDatesArray } = require('./../../src/utils/sortDatesArray')
+
+describe('sortDatesArray', () => {
+  it('should return the correct sorted array', () => {
+    // eslint-disable-next-line no-sparse-arrays
+    const datesArray = ['2021-01-01T12:00:00.000Z', '2021-01-01T09:00:00.000Z', '2021-01-02T12:00:00.000Z', , '2021-01-03T12:00:00.000Z']
+
+    const sortedDatesArray = sortDatesArray(datesArray)
+
+    console.log(sortedDatesArray)
+
+    expect(sortedDatesArray).toStrictEqual(['2021-01-01T09:00:00.000Z', '2021-01-01T12:00:00.000Z', '2021-01-02T12:00:00.000Z', '2021-01-03T12:00:00.000Z'])
+  })
+})
